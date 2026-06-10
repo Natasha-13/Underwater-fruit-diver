44d4b51a9339dc0c1676b52d6bff844c48bd2e4c
24239bhs <24239@burnside.school.nz>
2026-06-11 11:45:22 +1200

    main menu and options menu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
