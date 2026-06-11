382e779330b32c7dba9905cb131a7bb75be55e35
24239bhs <24239@burnside.school.nz>
2026-06-12 09:23:18 +1200

    Made options and credits menus

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,7 +5,7 @@ func _on_play_button_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
 
 
 func _on_credits_pressed() -> void:
