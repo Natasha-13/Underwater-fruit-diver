c14e86c6cf96110639fee92d5760144e1c87910a
24239bhs <24239@burnside.school.nz>
2026-06-12 18:48:40 +1200

    added music and edited options menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,11 +1,20 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $"MarginContainer/Main Buttons"
+@onready var options: Panel = $Options
+
+func _ready():
+	main_buttons.visible = true
+	options.visible = false
+
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 
 
 func _on_options_pressed() -> void:
 	print("options pressed")
+	main_buttons.visible = false
+	options.visible = true
 
 func _on_credits_pressed() -> void:
 	pass # Replace with function body.
@@ -13,3 +22,7 @@ func _on_credits_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_exit_options_pressed() -> void:
+	_ready()
