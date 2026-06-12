f9afcc9c5acb8e94a028716f3024c192abafc01c
24239bhs <24239@burnside.school.nz>
2026-06-12 17:33:58 +1200

    updated main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,8 +5,7 @@ func _on_play_button_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
-
+	print("options pressed")
 
 func _on_credits_pressed() -> void:
 	pass # Replace with function body.
