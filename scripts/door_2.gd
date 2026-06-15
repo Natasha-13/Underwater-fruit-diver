8427b3f3eae901589e6201c41ba9546611a8ce39
24239bhs <24239@burnside.school.nz>
2026-06-15 19:42:29 +1200

    Added fruit, a portal, and started a score

---
+++ scripts/door_2.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Diver":
+		print("Your Won!")
+		#load new level
+		get_tree().change_scene_to_file("res://scenes/you_won!.tscn")
+		pass
