76c1c444545546cd21122ee1b16200116bda59c3
24239bhs <24239@burnside.school.nz>
2026-07-21 14:39:04 +1200

    all the fruits disappear

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,12 +3,14 @@ extends Control
 @onready var main_buttons: VBoxContainer = $"MarginContainer/Main Buttons"
 @onready var options: Panel = $Options
 @onready var instructions: Panel = $Instructions
+@onready var credits: Panel = $Credits
 
 
 func _ready():
 	main_buttons.visible = true
 	options.visible = false
 	instructions.visible = false
+	credits.visible = false
 	
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
@@ -21,6 +23,8 @@ func _on_options_pressed() -> void:
 
 func _on_credits_pressed() -> void:
 	print("credits pressed")
+	main_buttons.visible = false
+	credits.visible = true
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
@@ -37,4 +41,8 @@ func _on_instructions_pressed() -> void:
 	
 
 func _on_exit_instructions_pressed() -> void:
+	_ready()
+
+
+func _on_exit_credits_pressed() -> void:
 	_ready()
