c26f3df96d113a1768d30706bc31f72c8f4b3169
24239bhs <24239@burnside.school.nz>
2026-07-22 12:44:38 +1200

    can pause game and quit game and go back to main menu

---
+++ scripts/level_1.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+@onready var pause_menu: Panel = $"CanvasLayer/pause menu"
+@onready var pause: Button = $CanvasLayer/Pause
+
+
+func _ready():
+	pause_menu.visible = false
+	pause.visible = true
+	
+
+func _on_pause_pressed() -> void:
+	print("pause button pressed")
+	pause_menu.visible = true
+	pause.visible = false
+
+
+func _on_resume_pressed() -> void:
+	print("resume button pressed")
+	_ready()
+
+func _on_quit_pressed() -> void:
+	print("quit button pressed")
+	get_tree().quit()
+	
+
+func _on_home_pressed() -> void:
+	print("home button pressed")
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+	
+	
