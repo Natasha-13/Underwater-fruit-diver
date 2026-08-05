5f3342aaa122e850b170abea0d5685e06da223f8
24239bhs <24239@burnside.school.nz>
2026-08-06 07:25:53 +1200

    Added some more art and changed player respawn point

--- scripts/diver.gd
+++ scripts/diver.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-var start_position = Vector2(500,200)
+var start_position = Vector2(440,-152)
 
 
 func _physics_process(delta: float) -> void:
