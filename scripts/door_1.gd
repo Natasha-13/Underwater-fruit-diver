b867e4e60b7f2a80af00cf0dca0212c04457f32f
24239bhs <24239@burnside.school.nz>
2026-08-10 12:05:07 +1200

    score has to be 10 to move on

--- scripts/door_1.gd
+++ scripts/door_1.gd
@@ -3,9 +3,13 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Diver":
-		if Global.score == 10:
+		if Global.score >= 10:
 			print("You Won!")
 			# load a new level
 			get_tree().change_scene_to_file("res://scenes/level_2.tscn")
 			pass
+		if Global.score <= 5:
+			print("you lost")
+			get_tree().change_scene_to_file("res://scenes/you_lost!.tscn")
+			
 	
--- scripts/door_2.gd
+++ scripts/door_2.gd
@@ -3,8 +3,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Diver":
-		if Global.score == 20:
+		if Global.score >= 20:
 			print("Your Won!")
 			#If score < 20 then change scene to you lost
 			get_tree().change_scene_to_file("res://scenes/you_won!.tscn")
 			pass
+		else:
+			print("try again")
