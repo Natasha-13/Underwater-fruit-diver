69ca3433eaa79dab9d9ab47b74e42b7751647b23
24239bhs <24239@burnside.school.nz>
2026-08-18 14:37:58 +1200

    chest opens when mouse enters it

--- scripts/chest.gd
+++ scripts/chest.gd
@@ -1,1 +1,17 @@
 extends Area2D
+
+@onready var closed: Sprite2D = $Closed
+@onready var opened: Sprite2D = $Opened
+
+
+func _ready():
+	closed.visible = true
+	opened.visible = false
+
+func _on_mouse_entered() -> void:
+	print("opened chest")
+	closed.visible = false
+	opened.visible = true
+
+func spawn_item() -> void:
+	var item = 
