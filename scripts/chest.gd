f8c08d392d9c4d6550bae63c6e4fb5213d1f5e74
24239bhs <24239@burnside.school.nz>
2026-08-19 12:44:26 +1200

    fruit only scores once and chest opens

--- scripts/chest.gd
+++ scripts/chest.gd
@@ -2,16 +2,31 @@ extends Area2D
 
 @onready var closed: Sprite2D = $Closed
 @onready var opened: Sprite2D = $Opened
-
+const GOLD_APPLE = preload("res://prefabs/gold_apple.tscn")
+var mouse_in = false
 
 func _ready():
 	closed.visible = true
 	opened.visible = false
 
 func _on_mouse_entered() -> void:
+	mouse_in = true
+
+func spawn_item() -> void:
+	var item = GOLD_APPLE.instantiate()
+	item.global_position = global_position + Vector2(0,-10)
+	get_tree().current_scene.add_child(item)
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if mouse_in:
+			open_chest()
+
+func _on_mouse_exited() -> void:
+	mouse_in = false
+
+func open_chest():
 	print("opened chest")
 	closed.visible = false
 	opened.visible = true
-
-func spawn_item() -> void:
-	var item = 
+	spawn_item()
--- scripts/gold_apple.gd
+++ scripts/gold_apple.gd
@@ -3,12 +3,11 @@ extends Area2D
 func _ready():
 	visible = true
 	$CollisionShape2D.disabled = false
-
+	
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Diver":
-		print("Yum!")
-		Global.score += 1
+		print("Yum")
+		Global.score += 2
 		visible = false
 		queue_free()
-		
