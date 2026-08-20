bb4b5b2a4bd2a9676c69ffef9a0c70a797ba00b7
24239bhs <24239@burnside.school.nz>
2026-08-20 13:31:18 +1200

    added code but doesn't work to instantiate fireworks to scene "you won!"

--- scripts/you_won!.gd
+++ scripts/you_won!.gd
@@ -2,3 +2,14 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+var firework_scene = preload("res://prefabs/fireworks.tscn")
+
+func spawn_firework(pos: Vector2):
+	var fw = firework_scene.instantiate()
+	fw.position = pos
+	add_child(fw)
+	fw.emitting = true
+	await get_tree().create_timer(2.0).timeout
+	fw.queue_free()
